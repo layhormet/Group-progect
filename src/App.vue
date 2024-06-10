d3d01fcb128640c5d32d8bd443060f8cfb3c2530
LAYHOR.MET <LAYHOR.MET@PNC.LAN>
2024-06-10 10:51:56 +0700

    add code

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavbarComVue/>
+ </template>
+ 
+ <script>
+ import NavbarComVue from "./components/NavbarView.vue"
+
+ export default {
+   name: "App",
+
+   components: {
+    NavbarComVue
+   }
+ }
+ </script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="row">
+    <div class="col-3" v-for="user in users" :key="user.id">
+      <CardCom :data="user"/>
+    </div>
+  </div>
+  <div class="row mt-3">
+    <AlertComVue v-for="user in users" :key="user.id" :name="user.name"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CardCom from '@/components/CardCom.vue'
+import AlertComVue from '@/components/AlertCom.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    CardCom,
+    AlertComVue
+  },
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Visal",
+          image: 'https://t3.ftcdn.net/jpg/06/17/13/26/360_F_617132669_YptvM7fIuczaUbYYpMe3VTLimwZwzlWf.jpg',
+          description: 'You now have a Vue 3 project set up with Vue Router and Bootstrap.'
+        },
+        {
+          id: 1,
+          name: "Chetra",
+          image: 'https://t3.ftcdn.net/jpg/06/17/13/26/360_F_617132669_YptvM7fIuczaUbYYpMe3VTLimwZwzlWf.jpg',
+          description: 'You now have a Vue 3 project set up with Vue Router and Bootstrap. '
+        },
+        {
+          id: 1,
+          name: "Dara",
+          image: 'https://t3.ftcdn.net/jpg/06/17/13/26/360_F_617132669_YptvM7fIuczaUbYYpMe3VTLimwZwzlWf.jpg',
+          description: 'You now have a Vue 3 project set up with Vue Router and Bootstrap. '
+        },
+        {
+          id: 1,
+          name: "Romdul",
+          image: 'https://t3.ftcdn.net/jpg/06/17/13/26/360_F_617132669_YptvM7fIuczaUbYYpMe3VTLimwZwzlWf.jpg',
+          description: 'You now have a Vue 3 project set up with Vue Router and Bootstrap. '
+        }
+      ]
+    }
+  }
+}
+</script>
